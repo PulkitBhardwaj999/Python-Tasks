5c2da49d668920e454ee3ea4668836d76e4c4f05
Pulkit bhradwaj <114151135+PulkitBhardwaj999@users.noreply.github.com>
2024-06-21 15:30:44 +0530

    Update Task 2.py

--- Task 2.py
+++ Task 2.py
@@ -1,3 +1,7 @@
+# Write a Python program that generates the Fibonacci sequence up to a specified number of terms, n. The Fibonacci sequence starts with 0 and 1, and each subsequent number in the
+# sequence is the sum of the two preceding numbers (e.g., 0, 1, 1, 2, 3, 5, 8, ...). Prompt the user to enter the number of terms (n) they want in the sequence and then display the
+# Fibonacci sequence up to that number of terms.
+
 def fibonacci_sequence(n):
     # Initialize first two terms
     fib_sequence = [0, 1]

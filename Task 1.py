344f4f83949a3dfe295e02b85f35546a87ec7287
Pulkit bhradwaj <114151135+PulkitBhardwaj999@users.noreply.github.com>
2024-06-21 15:31:49 +0530

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -1,4 +1,9 @@
 # Calculate Area with Conditions
+
+# Write a Python function calculate_area that takes two parameters: length and width. Itshould calculate and return the area of a rectangle. However, add a condition: if the length
+# is equal to the width, return "This is a square!" instead of the area. Then, write a program toinput values for length and width from the user and call the calculate_area function to
+# display either the area or the message.
+
 def calculate_area(length, width):
     if length == width:
         return "This is a square!"
